88fd91fa5b49e81e41ae296c767284fdc9bf5542
nameles <nameles@localhost.localdomain>
2017-03-03 16:40:09 +0900

    Add DB data

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from polls import views
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
